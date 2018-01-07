8b8199a461e1752dc12c7706b9237bbde31ed180
Revplit <34894997+Revplit@users.noreply.github.com>
2018-01-07 18:45:52 +0100

    Add files via upload

---
+++ php-response/all.php
@@ -0,0 +1,20 @@
+<?php
+  $result = "";
+    if(isset($_POST['sub'])) {
+      $ip = $_POST['ip'];
+      if(!empty($_POST['ip'])) {
+        $result = @file_get_contents("https://api.revplit.com/geo/all.php?ip=".$ip);
+      } else {
+           $result = "No IP filled in";
+        }
+       }
+?>
+
+<form method="POST">
+<p><b>IP:</b></p>
+<input type="text" name="ip" placeholder="127.0.0" />
+
+<input type="submit" value="Submit" name="sub"/>
+</form>
+
+<?php echo $result; ?>
